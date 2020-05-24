458de3c99c1bc8f4153bb4281655ed9d16f189dc
Hargobind Gupta <yara.developer@gmail.com>
2020-05-25 01:01:55 +0530

    addes bidding functions

--- contracts/ChitFund.sol
+++ contracts/ChitFund.sol
@@ -7,8 +7,8 @@ import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract ChitFundFactory {
     address[] public funds;
 
-    function createFund(string memory _fundName, uint _fundAmount, uint _noOfInstallments, uint _noOfInvestors) public {
-        ChitFund _newFund = new ChitFund(_fundName, _fundAmount, _noOfInstallments, _noOfInvestors, msg.sender);
+    function createFund(string memory _fundName, uint _installmentAmount, uint _noOfInstallments, uint _noOfInvestors) public {
+        ChitFund _newFund = new ChitFund(_fundName, _installmentAmount, _noOfInstallments, _noOfInvestors, msg.sender);
         address newFund= address(_newFund);
         funds.push(newFund);
     }
@@ -27,13 +27,16 @@ contract ChitFund {
     using SafeMath for uint;
 
     string public fundName;
-    uint public fundAmount;
+    uint public jackpot;
     uint public noOfInstallments;
     uint public noOfInvestors;
     address public manager;
     uint public fundBalance;
     uint public installmentAmount;
     uint public noOfInvestorsJoined;
+    address payable private winner =address(0);
+    uint public previusBid = 0;
+    uint public  winnersAmount;
 
 
 
@@ -42,7 +45,8 @@ contract ChitFund {
       bool gotJackpot;
       uint installementCounter;
       bool isReadytoInvest;
-      uint currentBidAmount;
+      bool canBid;
+      uint currentBidPercentage;
     }
 
     mapping(address => Investor) public investors;
@@ -51,14 +55,13 @@ contract ChitFund {
         require(msg.sender == manager,'Only ');
         _;
     }
-    constructor (string memory _fundName, uint _fundAmount, uint _noOfInstallments, uint _noOfInvestors, address _manager) public {
+    constructor (string memory _fundName, uint _installmentAmount, uint _noOfInstallments, uint _noOfInvestors, address _manager) public {
         fundName = _fundName;
-        fundAmount = _fundAmount * 1e18;
         noOfInstallments = _noOfInstallments;
         noOfInvestors = _noOfInvestors;
         manager = _manager;
-        uint tmp = SafeMath.mul(noOfInstallments,noOfInvestors);
-        installmentAmount = SafeMath.div(fundAmount,tmp);
+        installmentAmount = _installmentAmount * 1e18;
+        jackpot = SafeMath.mul(installmentAmount,noOfInvestors);
     }
 
     function getinstallmentAmount() public view returns (uint _amount) {
@@ -80,35 +83,46 @@ contract ChitFund {
         require(msg.value == installmentAmount,'Supplied amount is different from the Installment amount');
         require(investors[msg.sender].hasJoined,'You are not part of the current fund');
         require(investors[msg.sender].isReadytoInvest,'You Can invest after the jackpot is announced');
-        require(fundBalance < fundAmount,'Fund is full');
+        require(fundBalance < jackpot,'Fund is full');
         require(investors[msg.sender].installementCounter < noOfInstallments,'You have allready pail all the installments');
         investors[msg.sender].installementCounter += 1;
         investors[msg.sender].isReadytoInvest = false;
+        investors[msg.sender].canBid = true;
+
 
         fundBalance += msg.value;
     }
 
-    function biForJackpot(uint _bid) public {
-        investors[msg.sender].currentBidAmount = _bid;
+    function bidForJackpot(uint _bid) public {
+        require(investors[msg.sender].canBid,'You were a winner in previous biddings');
+        investors[msg.sender].currentBidPercentage = _bid;
         investors[msg.sender].isReadytoInvest = true;
+        uint previusBit = _bid;
+        if(_bid > previusBid){
+          winner = msg.sender;
+        }
+        previusBid = _bid;
+    }
+
+    function getWinner() view public returns (address) {
+      return winner;
+    }
+
+    function calculateWinnesJackpot(address _winner) public {
+      uint percentage = investors[_winner].currentBidPercentage;
+      uint x = SafeMath.mul(fundBalance,percentage);
+      uint y = SafeMath.div(x,100);
+      winnersAmount = fundBalance - y;
+      investors[_winner].canBid = false;
+
     }
 
 
-
-
-    function releaseFund() public isManager {
-      require(fundBalance == fundAmount,'Contributions required');
-
-          // uint index = random() % noOfInvestors;
-          address payable winner = msg.sender;
+    function releaseFund() public payable isManager {
+      require(fundBalance == jackpot,'Contributions required');
           winner.transfer(address(this).balance);
-
     }
 
-    // // Generating pseudo random number
-    // function random() private view returns (uint) {
-    //     return uint(keccak256(abi.encodePacked(block.difficulty, now, noOfInvestors)));
-    // }
 
 
 }
