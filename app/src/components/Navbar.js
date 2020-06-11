e155ebfe43c53aaf4d8360bfd2fbec9f0c2b177f
samarth30 <samarth.gugnani30@gmail.com>
2020-06-12 00:01:54 +0530

    added

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,35 +1,94 @@
 import React from "react";
 import Identicon from "identicon.js";
 
-const Navbar = ({ account }) => {
+const Navbar = ({ account, viewfund }) => {
   return (
-    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
-        Chit Fund
-      </a>
+    <header>
+      <div class="header-area header-transparent">
+        <div class="main-header header-sticky">
+          <div class="container-fluid">
+            <div class="row align-items-center">
+              <div class="col-xl-2 col-lg-2">
+                <h2>{viewfund[0]}</h2>
+              </div>
+              <div class="col-xl-10 col-lg-10 col-md-10">
+                <div class="menu-main d-flex align-items-center justify-content-end">
+                  <div class="main-menu f-right d-none d-lg-block">
+                    <nav>
+                      <ul id="navigation">
+                        <li class="active">
+                          <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                          <a href="about.html">About</a>
+                        </li>
+                        <li>
+                          <a href="services.html">Services</a>
+                        </li>
+                        <li>
+                          <a href="blog.html">Blog</a>
+                          <ul class="submenu">
+                            <li>
+                              <a href="blog.html">Blog</a>
+                            </li>
+                            <li>
+                              <a href="blog_details.html">Blog Details</a>
+                            </li>
+                            <li>
+                              <a href="elements.html">Element</a>
+                            </li>
+                            <li>
+                              <a href="apply.html">Apply Now</a>
+                            </li>
+                          </ul>
+                        </li>
+                        <li>
+                          <a href="contact.html">Contact</a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                  <div class="header-right-btn f-right d-none d-lg-block mb-3">
+                    <small id="account">{account}</small>{" "}
+                  </div>
+                </div>
+              </div>
 
-      <ul className="navbar-nav px-3">
-        <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-          <small className="text-white">
-            <small id="account">{account}</small>
-          </small>
+              <div class="col-12">
+                <div class="mobile_menu d-block d-lg-none"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    // <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+    //   <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">
+    //     Chit Fund
+    //   </a>
 
-          {account ? (
-            <img
-              className="ml-2"
-              width="30"
-              height="30"
-              src={`data:image/png;base64,${new Identicon(
-                account,
-                30
-              ).toString()}`}
-            />
-          ) : (
-            <span></span>
-          )}
-        </li>
-      </ul>
-    </nav>
+    //   <ul className="navbar-nav px-3">
+    //     <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+    //       <small className="text-white">
+    //         <small id="account">{account}</small>
+    //       </small>
+
+    //       {account ? (
+    //         <img
+    //           className="ml-2"
+    //           width="30"
+    //           height="30"
+    //           src={`data:image/png;base64,${new Identicon(
+    //             account,
+    //             30
+    //           ).toString()}`}
+    //         />
+    //       ) : (
+    //         <span></span>
+    //       )}
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 };
 
