df7131c1b5a8c26df936a8dcfe796a514f200689
samarth30 <samarth.gugnani30@gmail.com>
2020-06-15 08:49:31 +0530

    added

---
+++ app/src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div class="testimonial-area t-bg testimonial-padding">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-xl-11 col-lg-11 col-md-5">
+              <div class="h1-testimonial-active">
+                <div class="single-testimonial text-center">
+                  <div class="testimonial-caption">
+                    <div class="testimonial-top-cap">
+                      <img src="assets/img/gallery/testimonial.png" alt="" />
+                      <p>
+                        Logisti Group is a representative logistics operator
+                        providing full range of ser of customs clearance and
+                        transportation worl.
+                      </p>
+                    </div>
+
+                    <div class="testimonial-founder d-flex align-items-center justify-content-center">
+                      <div class="founder-img">
+                        <img
+                          src="assets/img/testmonial/Homepage_testi.png"
+                          alt=""
+                        />
+                      </div>
+                      <div class="founder-text">
+                        <span>Jessya Inn</span>
+                        <p>Co Founder</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
