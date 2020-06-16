fd736407400abfc1fb415ac5679f9acd1208bf2a
samarth30 <samarth.gugnani30@gmail.com>
2020-06-16 07:28:45 +0530

    added

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <div class="testimonial-area t-bg testimonial-padding">
+      {/* <div class="testimonial-area t-bg testimonial-padding">
         <div class="container">
           <div class="row d-flex justify-content-center">
             <div class="col-xl-11 col-lg-11 col-md-5">
@@ -37,7 +37,122 @@ function Footer() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
+      <footer>
+        <div class="footer-area">
+          <div class="container">
+            <div class="footer-top footer-padding">
+              <div class="row justify-content-between">
+                <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+                  <div class="single-footer-caption mb-50">
+                    <div class="single-footer-caption mb-30">
+                      <div class="footer-logo">
+                        <a href="index.html">
+                          <img src="assets/img/logo/logo2_footer.png" alt="" />
+                        </a>
+                      </div>
+                      <div class="footer-pera">
+                        <p>
+                          Heaven fruitful doesn't over lesser days appear
+                          creeping seasons so behold bearing
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-2 col-lg-2 col-md-4 col-sm-6">
+                  <div class="single-footer-caption mb-50">
+                    <div class="footer-tittle">
+                      <h4>Quick Link</h4>
+                      <ul>
+                        <li>
+                          <a href="#">About</a>
+                        </li>
+                        <li>
+                          <a href="#">Offers & Discounts</a>
+                        </li>
+                        <li>
+                          <a href="#">Get Coupon</a>
+                        </li>
+                        <li>
+                          <a href="#"> Contact Us</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                  <div class="single-footer-caption mb-50">
+                    <div class="footer-tittle">
+                      <h4>New Products</h4>
+                      <ul>
+                        <li>
+                          <a href="#">Woman Cloth</a>
+                        </li>
+                        <li>
+                          <a href="#">Fashion Accessories</a>
+                        </li>
+                        <li>
+                          <a href="#">Man Accessories</a>
+                        </li>
+                        <li>
+                          <a href="#">Rubber made Toys</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
+                  <div class="single-footer-caption mb-50">
+                    <div class="footer-tittle">
+                      <h4>Support</h4>
+                      <ul>
+                        <li>
+                          <a href="#">Frequently Asked Questions</a>
+                        </li>
+                        <li>
+                          <a href="#">Terms & Conditions</a>
+                        </li>
+                        <li>
+                          <a href="#"> Privacy Policy</a>
+                        </li>
+                        <li>
+                          <a href="#">Report a Payment Issue</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="footer-bottom">
+              <div class="row d-flex justify-content-between align-items-center">
+                <div class="col-xl-9 col-lg-8">
+                  <div class="footer-copy-right">
+                    <p></p>
+                  </div>
+                </div>
+                <div class="col-xl-3 col-lg-4">
+                  <div class="footer-social f-right">
+                    <a href="#">
+                      <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fas fa-globe"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fab fa-instagram"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
