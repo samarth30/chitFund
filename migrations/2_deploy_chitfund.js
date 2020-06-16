2d6b0db54670f380842fbb838ae966ebaf766232
samarth30 <samarth.gugnani30@gmail.com>
2020-06-16 07:29:24 +0530

    added

--- migrations/2_deploy_chitfund.js
+++ migrations/2_deploy_chitfund.js
@@ -1,5 +1,7 @@
 const ChitFund = artifacts.require("ChitFund");
+const ChitFundFactory = artifacts.require("ChitFundFactory");
 
-module.exports = function(deployer) {
-  deployer.deploy(ChitFund,'BeatInflation',1,3,3);
+module.exports = function (deployer) {
+  deployer.deploy(ChitFund, "BeatInflation", 1, 3, 3);
+  deployer.deploy(ChitFundFactory);
 };
