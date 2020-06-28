1b28af12bc422c95324efddec9f0b9c1417e22bf
samarth30 <samarth.gugnani30@gmail.com>
2020-06-28 10:52:07 +0530

    Updates

---
+++ precache-manifest.8d4474b2eca0fa288f022704df7d9bf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fa8427f1ae2d8d7abf2c47a3ad8633",
+    "url": "/chitFund/index.html"
+  },
+  {
+    "revision": "296f8ffa85687ffd5271",
+    "url": "/chitFund/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9d1840992276169f5295",
+    "url": "/chitFund/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "296f8ffa85687ffd5271",
+    "url": "/chitFund/static/js/2.b0b6d582.chunk.js"
+  },
+  {
+    "revision": "e07e577c585615d6cd1f7ed1e487dbe1",
+    "url": "/chitFund/static/js/2.b0b6d582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1840992276169f5295",
+    "url": "/chitFund/static/js/main.b85cb62b.chunk.js"
+  },
+  {
+    "revision": "ee7570d21c7423893e33",
+    "url": "/chitFund/static/js/runtime-main.650c8586.js"
+  }
+]);
